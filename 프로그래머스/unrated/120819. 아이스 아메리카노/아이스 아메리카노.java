bc102b0d39587f7fd530252e3d694971cb57a9fe
jaehkim <dnftksghkfkd@naver.com>
2024-01-10 09:07:56 +0900

    [level unrated] Title: 아이스 아메리카노, Time: 0.02 ms, Memory: 81 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] solution(int money) {
+        int[] answer = new int[2];
+        int count = money / 5500;
+        int change = money - count * 5500;
+        answer[0] = count;
+        answer[1] = change;
+        return answer;
+    }
+}
