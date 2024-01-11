99bb09a0d09e83e03768e6769c6b4f8b5fceef44
jaehkim <dnftksghkfkd@naver.com>
2024-01-11 09:18:09 +0900

    [level unrated] Title: 문자열 뒤집기, Time: 0.03 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/unrated/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,17 +1,5 @@
-
 class Solution {
     public String solution(String my_string) {
-        // StringBuilder를 사용하여 문자열을 뒤집습니다.
         return new StringBuilder(my_string).reverse().toString();
-    }
-
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-        
-        // 테스트를 위한 문자열
-        String testString = "Hello, World!";
-        
-        // 뒤집힌 문자열 출력
-        System.out.println(sol.solution(testString));
     }
 }
