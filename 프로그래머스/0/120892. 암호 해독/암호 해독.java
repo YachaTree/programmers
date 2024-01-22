cafea56159243a056f4a698df80b26a579a8fee6
jaehkim <dnftksghkfkd@naver.com>
2024-01-23 00:05:03 +0900

    [level 0] Title: 암호 해독, Time: 0.03 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(String cipher, int code) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = code - 1; i < cipher.length(); i += code) {
+            result.append(cipher.charAt(i));
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.solution("dfjardstddetckdaccccdegk", 4)); // "attack"
+        System.out.println(sol.solution("pfqallllabwaoclk", 2));         // "fallback"
+    }
+}
