eca3ff5f5866d2e2d07fef3c10b1ad55fb883002
jaehkim <dnftksghkfkd@naver.com>
2024-03-07 20:38:22 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 128 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -1,26 +1,25 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+        
         int n = Integer.parseInt(br.readLine());
-        int[] dp = new int[Math.max(n + 1, 3)]; // n이 1 또는 2일 수 있으므로, 배열 크기를 적어도 3으로 설정
-
-        dp[1] = 1;
-        dp[2] = 3;
-
+        if (n == 1) {
+            System.out.println(1);
+            return;
+        }
+        
+        int prev = 1; // dp[i-2]
+        int curr = 3; // dp[i-1]
         for (int i = 3; i <= n; i++) {
-            dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % 10007;
+            int next = (curr + 2 * prev) % 10007; // dp[i]
+            prev = curr;
+            curr = next;
         }
 
-        bw.write(dp[n] + "\n");
-        bw.flush(); // 버퍼의 내용을 출력
-        bw.close(); // 버퍼 닫기
+        System.out.println(curr);
     }
 }
