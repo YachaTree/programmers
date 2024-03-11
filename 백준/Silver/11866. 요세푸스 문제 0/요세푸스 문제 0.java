c2132d967102ceb30c2aa707b609fa2459d2d86e
jaehkim <dnftksghkfkd@naver.com>
2024-03-11 10:14:03 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 184 ms, Memory: 30924 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputs = br.readLine().split(" ");
+        int N = Integer.parseInt(inputs[0]);
+        int K = Integer.parseInt(inputs[1]);
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= N; i++) {
+            queue.offer(i);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append('<');
+
+        while (!queue.isEmpty()) {
+            for (int i = 0; i < K - 1; i++) {
+                int val = queue.poll();
+                queue.offer(val);
+            }
+            sb.append(queue.poll());
+            if (!queue.isEmpty()) {
+                sb.append(", ");
+            }
+        }
+
+        sb.append('>');
+        System.out.println(sb.toString());
+    }
+}
