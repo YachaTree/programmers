13083f3a2a0fd5fedc905247d4b81e37d1b3184a
jaehkim <dnftksghkfkd@naver.com>
2024-03-14 09:00:12 +0900

    [Silver V] Title: 단어 정렬, Time: 644 ms, Memory: 23876 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,37 +1,26 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt(); // 단어의 개수 N을 입력받음
-        scanner.nextLine(); // 숫자 입력 후 남은 개행 문자 제거
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
-        Set<String> words = new HashSet<>(); // 중복 제거를 위한 Set 생성
-
-        for (int i = 0; i < N; i++) {
-            words.add(scanner.nextLine()); // 단어 입력 받아 Set에 저장
-        }
-
-        // Set을 List로 변환하여 정렬 가능하게 함
-        List<String> sortedWords = new ArrayList<>(words);
-
-        // 조건에 맞게 정렬
-        Collections.sort(sortedWords, (word1, word2) -> {
+        Set<String> words = new TreeSet<>((word1, word2) -> {
             if (word1.length() == word2.length()) {
-                return word1.compareTo(word2); // 길이가 같으면 사전 순으로 정렬
+                return word1.compareTo(word2);
             } else {
-                return word1.length() - word2.length(); // 길이에 따라 정렬
+                return Integer.compare(word1.length(), word2.length());
             }
         });
 
-        // 정렬된 단어 출력
-        for (String word : sortedWords) {
-            System.out.println(word);
+        for (int i = 0; i < N; i++) {
+            words.add(br.readLine());
         }
+
+        words.forEach(System.out::println);
     }
 }
